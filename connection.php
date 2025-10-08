68b8574a578ae5383173a66ef9b4b631aa60133f
wiseman72 <wisemanmilimo@gmail.com>
2025-10-08 06:01:38 -0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,13 +4,11 @@ $username = "root";
 $password = "";
 $database = "user_system";
 
-try {
-    $conn = new mysqli($host, $username, $password, $database);
-    if ($conn->connect_error) {
-        throw new Exception("Connection failed: " . $conn->connect_error);
-    }
-} catch (Exception $e) {
-    error_log("Database connection error: " . $e->getMessage());
-    die("Error connecting to the database");
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to database $database!";
 }
 ?>
